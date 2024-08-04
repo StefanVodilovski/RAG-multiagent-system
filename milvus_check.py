d2d03773ca2459ca12bc2a49f91c195e548612e5
NineCvet <ninacvetkovska322@gmail.com>
2024-08-04 15:42:02 +0200

    Added printing of all collection entities.

--- milvus_check.py
+++ milvus_check.py
@@ -28,6 +28,16 @@ def list_all_collections():
     print(f"Existing collections: {collections}")
 
 
+def query_all_entities(collection_name):
+    try:
+        collection = Collection(name=collection_name)
+        results = collection.query(expr='id >= 0', output_fields=["id", "content"])
+        for result in results:
+            print(result)
+    except Exception as e:
+        print(f"Error querying collection: {e}")
+
+
 if __name__ == "__main__":
     # Set up a Milvus client
     connections.connect(
@@ -41,6 +51,9 @@ if __name__ == "__main__":
     # Check collection data
     collection_name = "google_2022"
     check_collection_data(collection_name)
+
+    # Print all collection entities
+    query_all_entities(collection_name)
 
     # List all existing collections
     list_all_collections()
