f46442396c64a4dc9387d8f237643f6ade1981f3
NineCvet <ninacvetkovska322@gmail.com>
2024-08-04 14:12:13 +0200

    Fixed requirements and generalized code for parsing.

--- llama_parser.py
+++ llama_parser.py
@@ -6,7 +6,7 @@ from dotenv import load_dotenv
 from pathlib import Path
 from llama_parse import LlamaParse
 from llama_index.core.node_parser import MarkdownElementNodeParser
-from llama_index.llms.huggingface_api import HuggingFaceInferenceAPI
+from llama_index.llms.huggingface import HuggingFaceInferenceAPI
 
 # Load api keys
 load_dotenv(Path(".env"))
@@ -36,10 +36,10 @@ def load_and_parse_documents():
     )
 
     # Load and parse documents
-    documents = [parser.load_data('google_data/google-2021-environmental-report.pdf'),
-                 parser.load_data('google_data/google-2022-environmental-report.pdf'),
-                 parser.load_data('google_data/google-2023-environmental-report.pdf'),
-                 parser.load_data('google_data/google-2024-environmental-report.pdf')]
+    documents = [
+        parser.load_data(f'google_data/google-{year}-environmental-report.pdf')
+        for year in range(2021, 2024)
+    ]
 
     llm = HuggingFaceInferenceAPI(
         model_name=config["hf_model_name"], token=HUGGINGFACE_API_KEY
@@ -55,16 +55,17 @@ def load_and_parse_documents():
 
 
 def create_embeddings(documents):
-    data = []
-
-    for document in documents:
+    for year, document in zip(range(2021, 2024), documents):
+        data = []
         for j, page in enumerate(document):
             embeddings = query(page.text)
-            data.append({"id": j, "vector": embeddings, "text": page.text})
+            if embeddings:
+                data.append({"id": j, "vector": embeddings, "text": page.text})
+
         df = pd.DataFrame(data)
-        csv_file = f'google_data/google_embeddings/google_{2021}.csv'
+        csv_file = f'google_data/google_embeddings/google_{year}.csv'
         df.to_csv(csv_file, index=False, encoding='utf-8')
-        data = []
+        print(f"Embeddings saved to {csv_file}")
 
 
 if __name__ == "__main__":
